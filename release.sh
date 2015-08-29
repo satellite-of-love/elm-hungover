b9acac23fbbbfd23c2c68c7a4c15d1f91d0bdf8b
Jessica Kerr <jessitron@outpace.com>
2015-08-29 02:12:39 +0000

    Initialize gh-pages, ready for elmduction

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ $# -lt 1 ]
+  then
+  echo "Usage: release.sh [commit hash]"
+  exit 1
+fi
+
+commit=$1
+srcdir=.
+main=FirstProject.elm
+
+git fetch
+git rm $srcdir/*.elm
+git checkout $commit .
+elm-make $main
+git add elm.js
+git add index.html
+git commit -m "Updated to $commit"
