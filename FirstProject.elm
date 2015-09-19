da812d7956d9ae5fe362764b06217c91a2bbef43
Jessica Kerr <jessitron@outpace.com>
2015-09-19 01:03:23 +0000

    more examples

--- FirstProject.elm
+++ FirstProject.elm
@@ -4,7 +4,20 @@ import Html exposing (Html, Attribute)
 import Html.Attributes as Attr
 
 main : Html
-main = Html.span [colorRed] [Html.text "hello friends"]
-
-colorRed : Attribute
-colorRed = Attr.attribute "style" "color:red;font-size:40px"
+main = 
+  Html.node "div" 
+    [] -- no attributes
+    [
+      Html.node "span" -- a node makes a <tag ...>...</tag>
+        [ -- attributes are the name-value pairs inside the opening tag, like <tag attribute=value>
+          Attr.attribute "style" "color:red;font-size:40px",
+          Attr.attribute "class" "banana"
+        ]
+        [  -- child elements are between <tag ...> and </tag>  
+          Html.node "p" []
+          [
+            Html.text "hello friends"  -- this is just typing, like words.
+          ]
+        ],
+      Html.text "I am outside the p and the span"
+    ]
