41151318da1c2cfee0b4882015b29100e76eb218
Jessica Kerr <jessitron@outpace.com>
2015-09-19 00:49:40 +0000

    red and big! bunny did it

--- FirstProject.elm
+++ FirstProject.elm
@@ -1,6 +1,10 @@
 module FirstProject where
 
-import Html exposing (Html)
+import Html exposing (Html, Attribute)
+import Html.Attributes as Attr
 
 main : Html
-main = Html.text "hello friends"
+main = Html.span [colorRed] [Html.text "hello friends"]
+
+colorRed : Attribute
+colorRed = Attr.attribute "style" "color:red;font-size:40px"
