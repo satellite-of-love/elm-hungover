11a1b5f2a32806308342f2f313933b53d43f1e0b
Tom Servo <rachelpage921@gmail.com>
2015-11-07 11:10:28 -0600

    two cat photos added

--- FirstProject.elm
+++ FirstProject.elm
@@ -1,12 +1,19 @@
 module FirstProject where
 
 import Html exposing (..)
+import Html.Attributes as Attr exposing (href)
+
 
 
 main : Html
 main = myDiv 
 
 pageHeader =
-    h1 [ ] [ Html.text "code code code"]
+    h1 [ ] 
+    [ a [href "http://www.thepaisleyelf.tumblr.com"]
+    [ Html.text "code code code"]
+    ]
 
-myDiv = div [ ] [ pageHeader, Html.text "hello friends"]
+myDiv = div [ ] [ pageHeader, Html.text "hello friends", image "images/cats_window.jpg", image "images/chill_cats.jpg"]
+
+image file = Html.img [Attr.src file] []
